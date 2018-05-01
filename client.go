e69ec639668fbb984148b3978faa5718e92d5f13
flywithbug <flywithbug@gmail.com>
2018-05-01 15:49:23 +0800

    即时通讯 client server 功能 ok  todo : 消息字节封装

--- client.go
+++ client.go
@@ -1,53 +1,8 @@
 package main
 
-
-import (
-	"bufio"
-	"log"
-	"net"
-	"os"
-	"io"
-	"fmt"
-)
+import "im_go/imc"
 
 func main() {
-
-	conn, err := net.Dial("tcp", "127.0.0.1:9000")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-	reader := bufio.NewReader(conn)
-	writer := bufio.NewWriter(conn)
-
-	in := bufio.NewReader(os.Stdin)
-
-	go func() {
-		for {
-			if line, _, err := reader.ReadLine(); err == nil {
-				log.Println(string(line),"------")
-
-			}else {
-				fmt.Println(err)
-				break
-			}
-		}
-	}()
-
-	for {
-		line, _, _ := in.ReadLine()
-		// 模拟一个请求
-		// {"command":"GET_CONN","data":null}
-		// {"command":"GET_BUDDY_LIST","data":null}
-		writer.WriteString(string(line) + "\n")
-		writer.Flush()
-	}
-
+	imc.StartClient(9000)
 }
 
-func ioCopy(dst io.Writer, src io.Reader) {
-	if _, err := io.Copy(dst, src); err != nil {
-		log.Fatal(err)
-	}
-}
--- imc/client.go
+++ imc/client.go
@@ -1,1 +1,43 @@
 package imc
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"os"
+	"fmt"
+)
+
+func StartClient(port int) {
+	address := fmt.Sprintf("0.0.0.0:%d",port)
+
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+	in := bufio.NewReader(os.Stdin)
+	go func() {
+		for {
+			if line, _, err := reader.ReadLine(); err == nil {
+				log.Println(string(line),"------")
+
+			}else {
+				fmt.Println(err)
+				break
+			}
+		}
+	}()
+
+	for {
+		line, _, _ := in.ReadLine()
+		// 模拟一个请求
+		// {"command":"GET_CONN","data":null}
+		// {"command":"GET_BUDDY_LIST","data":null}
+		writer.WriteString(string(line) + "\n")
+		writer.Flush()
+	}
+
+}
