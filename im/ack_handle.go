6ce8c52608902f6188731e9acd0e130a467eb766
flywithbug <flywithbug@gmail.com>
2018-05-18 15:10:04 +0800

    未发送消息单存入一个表中。

--- im/ack_handle.go
+++ im/ack_handle.go
@@ -5,22 +5,31 @@ import (
 	"im_go/model"
 )
 
+
+
 /*
 // 客户端返回的ack 用于标记消息已发送成功
 */
 func (client *Client) handleMessageACK(pro *Proto) {
-	//TODO 优化为rpc和方式修改
+	//TODO 优化为rpc和方式修改  通知发发送方，送达，（send operation）OP_MSG_DELIVER_ACK
 	var ack MessageACK
 	ack.FromData(pro.Body)
-	err := model.UpdateMessageACK(ack.msgId,1)
+	err := model.SaveIMMessageFromOfflineMessage(ack.msgId)
+	//err := model.UpdateMessageACK(ack.msgId,1)
 	if err != nil {
 		log.Error("error"+err.Error() + ack.Description())
 	}
+
 }
 
 //已读回执
 func (client *Client)handleMessageReadAck(pro *Proto)  {
-
+	var ack MessageACK
+	ack.FromData(pro.Body)
+	err := model.UpdateMessageACK(ack.msgId,2)
+	if err != nil {
+		log.Error("error"+err.Error() + ack.Description())
+	}
 }
 
 
