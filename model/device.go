17c92fcf1a47f7b3759a9ac85c370fe0509afdee
flywithbug <flywithbug@gmail.com>
2018-06-10 17:21:37 +0800

    no message

--- model/device.go
+++ model/device.go
@@ -12,17 +12,18 @@ type Device struct {
 	DeviceId		string		`json:"device_id"`
 	Platform		int			`json:"platform"`   //1.iOS, 2.android
 	Description		string		`json:"description"`
+	UserId 			string		`json:"user_id"`  //绑定的用户UserId
 }
 
-func SaveDeviceInfo(token ,deviceId,description string,platformType int)error  {
-	stmt,err :=Database.Prepare("INSERT into im_device SET device_id=? ON DUPLICATE key UPDATE ,device_token=?,platform=?,description=?")
+func SaveDeviceInfo(token ,deviceId,description string,platformType int,userId string)error  {
+	stmt,err :=Database.Prepare("INSERT into im_device SET user_id=? ON DUPLICATE key UPDATE ,device_id,device_token=?,platform=?,description=?")
 	if err != nil{
 		log.Warn(err.Error())
 		err = errors.New("服务错误")
 		return err
 	}
 	defer stmt.Close()
-	_,err = stmt.Exec(deviceId,token,platformType,description)
+	_,err = stmt.Exec(userId,deviceId,token,platformType,description)
 	if err!= nil {
 		log.Warn(err.Error())
 		err = errors.New("device_id not found")
@@ -31,10 +32,10 @@ func SaveDeviceInfo(token ,deviceId,description string,platformType int)error  {
 	return nil
 }
 
-func GetDeviceBy(deviceId string)(*Device,error)  {
+func GetDeviceByUserId(userId string)(*Device,error)  {
 	var device Device
-	row := Database.QueryRow("SELECT id,device_id,device_token,platform,description FROM im_device WHERE device_id = ?",deviceId)
-	err := row.Scan(&device.Id,&device.DeviceId,&device.DeviceToken,&device.Platform,&device.Description)
+	row := Database.QueryRow("SELECT id,user_id,device_id,device_token,platform,description FROM im_device WHERE user_id = ?",userId)
+	err := row.Scan(&userId,&device.Id,&device.DeviceId,&device.DeviceToken,&device.Platform,&device.Description)
 	if err != nil {
 		return nil,&DatabaseError{"未查询到该设备"}
 	}
