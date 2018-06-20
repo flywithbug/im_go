0c3e1a1f071faf54692d274e28e75f6ed916667b
flywithbug <flywithbug@gmail.com>
2018-06-20 16:06:42 +0800

    网络请求

---
+++ http/http_request.go
@@ -0,0 +1,69 @@
+package http
+
+
+import (
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/flywithbug/log4go"
+	"bytes"
+	"fmt"
+)
+
+
+func GET(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func POST(url string,v interface{},header map[string]string) ([]byte, error)  {
+	j,err := json.Marshal(v)
+	fmt.Printf(string(j))
+	if err !=nil {
+		log4go.Error(err.Error())
+		return nil,err
+	}
+	req , err := http.NewRequest("POST",url,bytes.NewBuffer(j))
+	for k,v := range header  {
+		req.Header.Set(k,v)
+	}
+	client := &http.Client{}
+	resp ,err := client.Do(req)
+	if err != nil{
+		log4go.Error(err.Error())
+		return nil,err
+	}
+	defer resp.Body.Close()
+	body,err := ioutil.ReadAll(resp.Body)
+	return body,err
+}
+
+func PUT(url string,v interface{},header map[string]string) ([]byte, error)  {
+	j,err := json.Marshal(v)
+	log4go.Info(string(j))
+	if err !=nil {
+		log4go.Error(err.Error())
+		return nil,err
+	}
+	req , err := http.NewRequest("PUT",url,bytes.NewBuffer(j))
+	for k,v := range header  {
+		req.Header.Set(k,v)
+	}
+	client := &http.Client{}
+	resp ,err := client.Do(req)
+	if err != nil{
+		log4go.Error(err.Error())
+		return nil,err
+	}
+	defer resp.Body.Close()
+	body,err := ioutil.ReadAll(resp.Body)
+	return body,err
+}
