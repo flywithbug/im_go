954eec954ba7614c4c395a63c79e237476f918b4
flywithbug <flywithbug@gmail.com>
2018-06-20 16:07:19 +0800

    推送处理

--- im/connection.go
+++ im/connection.go
@@ -104,6 +104,7 @@ func (client *Connection) SendMessage(uid int32, pro *Proto) bool {
 	if len(clients) == 0 {
 		//走推送通道
 		log.Debug(fmt.Sprintf("TODO(need offline push )can't send message, appid:%d uid:%d cmd:%d", appid, uid, pro.Operation))
+		PushServiceHandler(uid,appid,pro)
 		return false
 	}
 
