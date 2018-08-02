3465c2a5639faf654f6955098b0e8e9c708ce398
flywithbug <flywithbug@gmail.com>
2018-08-03 04:19:52 +0800

    邮箱校验

--- mail/mail_handle.go
+++ mail/mail_handle.go
@@ -4,16 +4,18 @@ import (
 	"gopkg.in/gomail.v2"
 	"im_go/config"
 	"errors"
+	"regexp"
+	"strings"
 )
 
 var(
 	Mail *gomail.Dialer
 )
 
-func SendMail(to ,title ,from string,mType int,body string) error  {
+func sendMail(to ,title ,subject string,mType int,body string) error  {
 	m := gomail.NewMessage()
 	m.SetAddressHeader("From", config.Conf().MailConfig.Username,title)
-	m.SetHeader("Subject", from)
+	m.SetHeader("Subject", subject)
 	m.SetHeader("To", to)
 	m.SetBody("text/html", body)
 
@@ -23,7 +25,13 @@ func SendMail(to ,title ,from string,mType int,body string) error  {
 	return Mail.DialAndSend(m)
 }
 
-func SendVerifyMail(uuid,mail string)  {
-	//verifyStr := "www.flywithme.top:"
+func SendVerifyMail(uuid,mail string) error {
+	verifyStr := "http://www.flywithme.top/im?uuid=" + uuid
+	return sendMail(mail,"足迹","邮箱验证",0,verifyStr)
 }
 
+var routerRe = regexp.MustCompile(`^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$`)
+func MailStringVerify(mail string)bool  {
+	match := routerRe.FindString(mail)
+	return strings.EqualFold(match,mail)
+}
